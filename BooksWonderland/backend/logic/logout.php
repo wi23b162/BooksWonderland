b0deb989562ee6ea9477b9f33e7803543c423928
wi23b165 <wi23b165@technikum-wien.at>
2025-04-29 12:02:19 +0200

    implement logout functionality

--- BooksWonderland/backend/logic/logout.php
+++ BooksWonderland/backend/logic/logout.php
@@ -1,6 +1,13 @@
 <?php
 session_start();
+
+session_unset();
 session_destroy();
-setcookie("user_id", "", time() - 3600, "/");
+
+if (isset($_COOKIE['user_id'])) {
+    
+    setcookie("user_id", "", time() - 3600, "/", "", true, true);  
+}
+   
 header("Location: ../../frontend/login.html");
 exit;
